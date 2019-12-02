8a2c07764fde3e8b732a3e3f95e20cbfda7bf288
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-12-01 23:55:12 -0500

    Remove extraneous code

--- lib/bookmarks.py
+++ lib/bookmarks.py
@@ -42,24 +42,6 @@ db.create_tables([Bookmark])
 # query = Bookmark.get(Bookmark.name == 'Google').name
 # print(query)
 
-# def intro(): 
-#     print("Welcome to the bookmark collector app. You have 5 options. Select 'c' to create a new bookmark, 'r' to see all bookmarks, 'u' to update an existing bookmark, 'd' to delete a bookmark, or 's' to search for a specific bookmark" )
-#     choice = input('What is your choice?')
-#     if choice == 'c':
-#         create()
-#     elif choice == 'r': 
-#         read()
-#     elif choice == 'u': 
-#         update()
-#     elif choice == 'd': 
-#         delete()
-#     elif choice == 's': 
-#         read_one()
-#     else: 
-#         print('Sorry that is not a valid choice, please try again')
-#         intro()
-
-
 
 root = Tk()
 root.title('Bookmark Collector')
@@ -212,68 +194,8 @@ query_one_button = Button(root, text="Search Bookmark Name", command=query_one)
 query_one_button.grid(row=14, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
 
 
-# def create(): 
-#     print("Each bookmark must include: the name of the website, the website url, and details about the bookmark")
-#     new_name = input('What is the name of the website?')
-#     new_url = input('What is the url of the website?')
-#     new_details = input('Tell me a little bit  about this website')
-#     new_name = Bookmark(name=new_name, url=new_url, details=new_details)
-#     new_name.save()
-#     print('Your bookmark has been added!')
-
-    
-# def read():
-#     query = Bookmark.select()
-#     for site in query: 
-#         print(site.name, '->', site.details, ' ->', site.url)
-
-# def read_one(): 
-#     name = input('Please enter the name of the website you are looking for')
-#     record_name = Bookmark.get(Bookmark.name == name).name
-#     record_url = Bookmark.get(Bookmark.name == name).url
-#     record_details = Bookmark.get(Bookmark.name == name).details
-#     print(record_name, '->', record_details, '->', record_url)
-
-# def update(): 
-#     print('To update a bookmark from our collection, please enter the name of the website you would like to update')
-#     name = input('What is the name of the website?')
-#     record = Bookmark.get(Bookmark.name == name)
-#     print(f"How would you like to update '{name}'?")
-#     choice = input("Select 'n' to update website name, 'u' to update website url, or 'd' to update website details")
-#     if choice == 'n': 
-#         new_name = input('Please enter new name of website')
-#         record.name = new_name
-#         record.save()
-#         print(f"The new name for {name} is now {new_name}")
-#     elif choice == 'u': 
-#         new_url = input('Please enter the new url of the website')
-#         record.url = new_url
-#         record.save()
-#         print(f"The new url for {name} is now {new_url}")
-#     elif choice == 'd': 
-#         new_details = input('Please enter the new details of the website')
-#         record.details = new_details
-#         record.save()
-#         print(f"The new details for {name} are {new_details}")
-#     else: 
-#         option = input("Sorry that was not a valid choice. Select 'y' to try again or 'n' to leave bookmark collection" )
-#         if option == 'y': 
-#             update()
-#         elif option == 'n': 
-#             print('Goodbye!')
-#         else: 
-#             pass 
-
-
-# def delete(): 
-#     print('To delete a bookmark from our collection, please enter the name of the website you would like to delete')
-#     name = input('what is the name of the website?')
-#     record = Bookmark.get(Bookmark.name == name)
-#     record.delete_instance()
-#     print(f"{name} has been deleted")
-
 root.mainloop()
-# intro()
+
 
 
 
